b2223f833fee15fc1d32d51f0796b04a55c8f8ce
KaushalBhatol <kaushal.bhtol@outlook.com>
2021-04-13 17:53:13 +0530

    operators

--- week_1/08 Oprators-2 logical and Bitwise.c
+++ week_1/08 Oprators-2 logical and Bitwise.c
@@ -3,21 +3,25 @@
 int main(int argc, char const *argv[])
 {
     int a, b;
-    // Logical operators
-    printf("\n=>Logical operators\n");
     // input from user
     printf("\nNote :Enter Values 0 or 1 only!:\na = ");
     scanf("%d", &a);
     printf("b = ");
     scanf("%d", &b);
     printf("Enterd values are : a = %d and b = %d\n", a, b);
-    // output 
+
+    // Logical operators
+    printf("\n=>01 Logical operators\n");
     printf("a AND b = a&&b = %d\n", a&&b);
     printf("a OR b = a||b = %d\n", a||b);
     printf("NOT a = !a = %d\n",!a);
     printf("NOT b = !b = %d\n",!b);
 
-
+    // Bitwise operators
+    printf("\n=>02 Bitwise operators\n");
+    printf("a AND b = %d\n", a&b);
+    printf("a OR b = %d\n", a|b);
+    printf("a XOR b = %d .. when both a and b values are true and false it give true \n ",a^b);
     
     return 0;
 }
