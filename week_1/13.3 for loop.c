1eef7d3b8ffb63874b4140292db6c248ad6bfa94
Kaushal Bhatol <kaushal.bhatol@outlook.com>
2021-04-17 21:29:31 +0530

    loop

--- week_1/13.3 for loop.c
+++ week_1/13.3 for loop.c
@@ -5,13 +5,11 @@ int main()
     // a = 0;
     // b = 2;
     printf("a b\n");
-
     for (a = 0, b = a+1; a <= 5; a++, b++)
     // for (; a < 5, b < 10; a++, b++)
     {
         printf("%d %d\n", a, b);
         // b = b+1;
     }
-
     return 0;
 }
